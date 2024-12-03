90c5d9a172049a2661e3a3c179363b27a447293e
DevanceDD <devance_demoday@outlook.com>
2024-12-03 00:17:50 -0300

    Delete Devance-main directory

--- Devance-main/js/script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".menu")
-
-const openMenu = () => {
-    const navbar = document.querySelector(".navbar")
-    navbar.classList.toggle("open-menu")
-}
-
-menu.addEventListener("click", openMenu)
